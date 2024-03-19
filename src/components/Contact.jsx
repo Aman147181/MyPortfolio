36843b086b226069b919a90bfbe06f16506d4968
Aman Shrestha <amanshrestha147181@gmail.com>
2024-03-19 17:04:38 +0545

    some changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="customsm:min-h-screen z-10 flex items-center px-2 py-10 justify-center">
+    <div className="min-h-screen z-10 flex items-center px-2 py-10 justify-center">
       <div className="box border-white border-2 py-16 overflow-hidden rounded-[36px] sm:rounded-[56px] flex flex-col space-y-1 items-center px-10 sm:px-32 justify-center relative">
         <h1 className="font-roboto text-white opacity-1 z-50 sm:pl-3 text-base customsm:text-xl sm:text-[24px] leading-[5px] pb-5 uppercase">
           got a project in mind?{" "}
