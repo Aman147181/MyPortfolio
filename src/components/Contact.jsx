d6e54cc80eb411c4c508274842b4a98489b3dc74
Aman Shrestha <amanshrestha147181@gmail.com>
2024-03-19 17:00:57 +0545

    some changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="min-h-screen z-10 flex items-center justify-center">
-      <div className="box border-white border-2 py-24 overflow-hidden rounded-[56px] flex flex-col space-y-1 items-center px-10 sm:px-32 justify-center relative">
-        <h1 className="font-roboto text-white opacity-1 z-50 sm:pl-3 text-base customsm:text-xl sm:text-[24px] leading-[5px] pb-2 uppercase">
+    <div className="customsm:min-h-screen z-10 flex items-center px-2 py-10 justify-center">
+      <div className="box border-white border-2 py-16 overflow-hidden rounded-[36px] sm:rounded-[56px] flex flex-col space-y-1 items-center px-10 sm:px-32 justify-center relative">
+        <h1 className="font-roboto text-white opacity-1 z-50 sm:pl-3 text-base customsm:text-xl sm:text-[24px] leading-[5px] pb-5 uppercase">
           got a project in mind?{" "}
         </h1>
         <h1 className="font-roboto font-light text-white z-50 text-5xl opacity-1 text-center customsm:text-7xl sm:text-[90px] md:text-[120px] pb-12 uppercase">
