f63ab97a08dc7ba48daa733b6f76e19aec9dc531
Aman Shrestha <amanshrestha147181@gmail.com>
2024-03-19 16:48:26 +0545

    some changes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -10,18 +10,18 @@ const Work = () => {
         <h1 className="font-truculenta pt-2 text-base sm:text-xl md:text-2xl lg:text-4xl sm:pl-24   ">Here are some of my projects</h1>
       </div>
       <div className="flex  items-center  overflow-y-auto scrollbar-hide   py-10  gap-8 pt-10 customsm:px-10 z-20 lg:px-24">
-        <div className="w-full hover:h-[560px] hover:w-[110%] duration-700 max-w-4xl min-w-[314px] customsm:min-w-[360px]  rounded-[44px] h-[460px]  customsm:h-[520px] relative overflow-y-hidden ">
+        <div className="w-full customsm:hover:h-[560px] customsm:hover:w-[110%] duration-700 max-w-4xl min-w-[314px] customsm:min-w-[360px]  rounded-[44px] h-[460px]  customsm:h-[520px] relative overflow-y-hidden ">
           <a href="https://urban-dwellings.vercel.app/">
             <img className=" " src="/urban.png" alt="Urban Dwellings" />
           </a>
         </div>
 
-        <div className="w-full max-w-4xl hover:h-[560px] hover:w-[110%]  duration-700 min-w-[314px] customsm:min-w-[360px] rounded-[44px]  h-[460px]  customsm:h-[520px] relative  overflow-hidden ">
+        <div className="w-full max-w-4xl customsm:hover:h-[560px] customsm:hover:w-[110%]  duration-700 min-w-[314px] customsm:min-w-[360px] rounded-[44px]  h-[460px]  customsm:h-[520px] relative  overflow-hidden ">
           <a href="https://github.com/Aman147181/minor-project">
             <img className="" src="/safari.png" alt="Safari Nepal " />
           </a>
         </div>
-        <div className="w-full max-w-4xl hover:h-[560px] hover:w-[110%] duration-700 min-w-[314px] customsm:min-w-[360px] rounded-[44px] h-[460px]  customsm:h-[520px] relative  overflow-hidden ">
+        <div className="w-full max-w-4xl customsm:hover:h-[560px] customsm:hover:w-[110%] duration-700 min-w-[314px] customsm:min-w-[360px] rounded-[44px] h-[460px]  customsm:h-[520px] relative  overflow-hidden ">
           <a href="https://ayushachary.com.np/">
             <img className="" src="/rupse.png" alt="Rupse Holidays" />
           </a>
