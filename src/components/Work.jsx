b703b6601c8070f19b44cbd064f8c2a11b4257ec
Aman Shrestha <amanshrestha147181@gmail.com>
2024-03-21 14:23:43 +0545

    almost finish

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -12,10 +12,10 @@ const Work = () => {
         </h1>
       </div>
       <div className="flex  items-center  overflow-y-auto scrollbar-hide   py-10  gap-8 pt-5  z-20 lg:px-24">
-        <div className="flex flex-col space-y-1">
-          <div className="w-full sm:hover:h-[520px] sm:hover:w-[105%] duration-700 max-w-4xl min-w-[314px] customsm:min-w-[360px] rounded-3xl  customsm:rounded-[44px] h-[480px]  sm:h-[480px] relative overflow-y-hidden ">
+      <div className="flex flex-col space-y-1">
+          <div className="w-full sm:hover:h-[500px] sm:hover:w-[105%] duration-700 max-w-4xl min-w-[314px] customsm:min-w-[360px] rounded-3xl  customsm:rounded-[44px] h-[480px]   relative overflow-y-hidden ">
             <a href="https://urban-dwellings.vercel.app/">
-              <img className=" " src="/urban.png" alt="Urban Dwellings" />
+              <img className="image" src="/urban.png" alt="Urban Dwellings" />
             </a>
           </div>
           <div className="flex justify-between  px-5 sm:hidden leading-5 text-xl pt-3 font-truculenta">
@@ -24,10 +24,11 @@ const Work = () => {
           </div>
         </div>
 
+
         <div className="flex flex-col space-y-1">
-          <div className="w-full sm:hover:h-[520px] sm:hover:w-[105%] duration-700 max-w-4xl min-w-[314px] customsm:min-w-[360px] rounded-3xl  customsm:rounded-[44px] h-[480px]  sm:h-[480px] relative overflow-y-hidden ">
+          <div className="w-full sm:hover:h-[500px] sm:hover:w-[105%] duration-700 max-w-4xl min-w-[314px] customsm:min-w-[360px] rounded-3xl  customsm:rounded-[44px] h-[480px]  sm:h-[480px] relative overflow-y-hidden  ">
             <a href="https://github.com/Aman147181/minor-project">
-              <img className="" src="/safari.png" alt="Safari Nepal " />
+              <img className="image" src="/safari.png" alt="Safari Nepal " />
             </a>
           </div>
           <div className="flex justify-between  px-5 sm:hidden leading-5  text-xl pt-3 font-truculenta">
@@ -36,9 +37,9 @@ const Work = () => {
           </div>
         </div>
         <div className="flex flex-col space-y-1">
-          <div className="w-full sm:hover:h-[520px] sm:hover:w-[105%] duration-700 max-w-4xl min-w-[314px] customsm:min-w-[360px] rounded-3xl  customsm:rounded-[44px] h-[480px]  sm:h-[480px] relative overflow-y-hidden ">
+          <div className="w-full sm:hover:h-[500px] sm:hover:w-[105%] duration-700 max-w-4xl min-w-[314px] customsm:min-w-[360px] rounded-3xl  customsm:rounded-[44px] h-[480px]  sm:h-[480px] relative overflow-y-hidden ">
             <a href="https://ayushachary.com.np/">
-              <img className="" src="/rupse.png" alt="Rupse Holidays" />
+              <img className="image" src="/rupse.png" alt="Rupse Holidays" />
             </a>
           </div>
           <div className="flex justify-between  px-5 sm:hidden leading-5  text-xl pt-3 font-truculenta">
